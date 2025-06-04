1cb14cf22807d17a131b6c93d4b80db23ef91874
ipyton <czhdawang@163.com>
2025-06-04 10:56:32 +1000

    debug for notification service

--- Notification/src/main/java/com/chen/notification/configs/WebSocketConfig.java
+++ Notification/src/main/java/com/chen/notification/configs/WebSocketConfig.java
@@ -3,7 +3,9 @@ package com.chen.notification.configs;
 import jakarta.websocket.server.ServerEndpointConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Component
 public class WebSocketConfig extends ServerEndpointConfig.Configurator implements ApplicationContextAware {
@@ -18,5 +20,9 @@ public class WebSocketConfig extends ServerEndpointConfig.Configurator implement
     @Override
     public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
         return applicationContext.getBean(endpointClass);
+    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
