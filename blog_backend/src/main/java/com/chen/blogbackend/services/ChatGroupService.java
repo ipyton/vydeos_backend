ab98a1fa423fd9fc7d6214aae54e9cf5686b0fac
ipyton <56996660+ipyton@users.noreply.github.com>
2025-07-24 15:39:27 +1000

    update attributes

--- blog_backend/src/main/java/com/chen/blogbackend/services/ChatGroupService.java
+++ blog_backend/src/main/java/com/chen/blogbackend/services/ChatGroupService.java
@@ -85,7 +85,7 @@ public class ChatGroupService {
             getGroupMember = session.prepare("select * from group_chat.chat_group_members_by_group where group_id = ? and user_id = ?");
             getGroupOwner = session.prepare("select owner_id from group_chat.chat_group_details where group_id = ?;");
             updateGroupDetails = session.prepare("UPDATE group_chat.chat_group_details SET introduction = ?, name = ?, allow_invite_by_token = ? WHERE group_id = ?;");
-            insertInvitation = session.prepare("insert into group_chat.invitations (sourceId, expire_time, code, userId, create_time) values (?, ?, ?, ?, ?)");
+            insertInvitation = session.prepare("insert into group_chat.invitations (source_id, expire_time, code, userId, create_time) values (?, ?, ?, ?, ?)");
             getInvitation = session.prepare("select * from group_chat.invitations where code = ?;");
             removeGroupMemberByGroup = session.prepare("delete from group_chat.chat_group_members_by_group where user_id = ? and group_id = ?;");
             removeGroupMemberByUser =session.prepare("delete from group_chat.chat_group_members_by_user where user_id = ? and group_id = ?;");
